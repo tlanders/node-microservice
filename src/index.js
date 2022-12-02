e56b259613e534c29ca744ac0ee629dc587d7e5c
Troy Landers <tlanders@landersconsulting.biz>
2022-12-01 19:35:25 -0600

    added nodemon and todos endpoint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const todos = [
+    {
+        userId: 1,
+        id: 19,
+        title: "molestiae ipsa aut voluptatibus pariatur dolor nihil",
+        completed: true
+    },
+    {
+        userId: 1,
+        id: 20,
+        title: "ullam nobis libero sapiente ad optio sint",
+        completed: true
+    },
+    {
+        userId: 2,
+        id: 21,
+        title: "suscipit repellat esse quibusdam voluptatem incidunt",
+        completed: false
+    },
+    {
+        userId: 2,
+        id: 22,
+        title: "distinctio vitae autem nihil ut molestias quo",
+        completed: true
+    }
+];
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
+
+app.get('/todos', (req, res) => {
+    res.send("Get Todos!");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
